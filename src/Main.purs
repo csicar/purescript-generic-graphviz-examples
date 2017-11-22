37b0ffa09df325409bab4274a28223eddbc5713e
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2017-11-22 16:40:16 +0100

    initial

---
+++ src/Main.purs
@@ -0,0 +1,77 @@
+module Main where
+
+import Control.Monad.Eff (Eff)
+import Control.Semigroupoid ((>>>))
+import Data.Array (range)
+import Data.DotLang (class DotR)
+import Data.Foldable (foldr)
+import Data.Generic.Rep (class Generic)
+import Data.GenericGraph (class Edges, genericEdges, genericToDot)
+import Data.Semigroup ((<>))
+import Data.String (Pattern(..), Replacement(..), replaceAll)
+import Global (encodeURIComponent)
+import Graphics.Graphviz (Engine(..), renderToSvg)
+import Prelude hiding (div)
+import Pux (CoreEffects, EffModel, start)
+import Pux.DOM.Events (onClick)
+import Pux.DOM.HTML (HTML)
+import Pux.Renderer.React (renderToDOM)
+import Text.Smolder.HTML (button, div, img, pre, span)
+import Text.Smolder.HTML.Attributes (src)
+import Text.Smolder.Markup (text, (!), (#!))
+
+--Simple
+data Simple = A | B
+
+derive instance genericSimple :: Generic Simple _
+instance simpleToDot :: DotR Simple where toDot = genericToDot
+
+instance simpleEdges :: Edges Simple where edges =  genericEdges
+
+--List
+data List' a = Nil | Cons' a (List' a)
+
+derive instance listGeneric :: Generic (List' a) _
+
+instance listEdges :: Edges a => Edges (List' a) where edges x = genericEdges x
+
+fromArray :: ∀a. Array a -> List' a
+fromArray = foldr Cons' Nil
+
+fromInt :: Int -> List' Int
+fromInt = range 0 >>> fromArray
+
+
+data Event = Increment | Decrement
+
+type State = Int
+
+-- | Return a new state (and effects) from each event
+foldp :: ∀ fx. Event -> State -> EffModel State Event fx
+foldp Increment n = { state: n + 1, effects: [] }
+foldp Decrement n = { state: n - 1, effects: [] }
+
+stuffify d = encodeURIComponent $ renderToSvg  Dot $ genericToDot d
+
+-- | Return markup from the state
+view :: State -> HTML Event
+view count =
+  div do
+    div do
+      button #! onClick (const Increment) $ text "Increment"
+      span $ text (show count)
+      button #! onClick (const Decrement) $ text "Decrement"
+    div do
+      img ! src ("data:image/svg+xml;charset=utf8," <> stuffify (fromInt count))
+
+-- | Start and render the app
+main :: ∀ fx. Eff (CoreEffects fx) Unit
+main = do
+  app <- start
+    { initialState: 0
+    , view
+    , foldp
+    , inputs: []
+    }
+
+  renderToDOM "#app" app.markup app.input
